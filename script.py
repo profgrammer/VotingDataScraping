a1fdba2c52bbdbd7493ffd3b4b558ed8ca4d00c2
Divya Khetan <divya.khetan@somaiya.edu>
2019-06-01 12:43:27 +0530

    Update script.py
    added headings for csv

--- script.py
+++ script.py
@@ -28,7 +28,7 @@ for row in reader:
 
 fw = open('2009result.csv', 'w')
 
-d = ""
+d = "2009_Constituency_Number, 2009_Constituency_Name, 2009_Candidate_Name_1, 2009_Party_1, 2009_Votes_1, 2009_Percentage_Votes_1, 2009_Position_1, 2009_Candidate_Name_2, 2009_Party_2, 2009_Votes_2, 2009_Percentage_Votes_2, 2009_Position_2, 2009_Candidate_Name_3, 2009_Party_3, 2009_Votes_3, 2009_Percentage_Votes_3, 2009_Position_3, 2009_Candidate_Name_4, 2009_Party_4, 2009_Votes_4, 2009_Percentage_Votes_4, 2009_Position_4, 2009_Candidate_Name_5, 2009_Party_5, 2009_Votes_5, 2009_Percentage_Votes_5, 2009_Position_5" + "\n"
 for key in data:
     d = d + data[key]["2009_Constituency_Number"] + ", " + data[key]["2009_Constituency_Name"] +", " + data[key]["2009_Candidate_Name_1"] + ", " + data[key]["2009_Party_1"] + ", " + data[key]["2009_Votes_1"] + ", " + data[key]["2009_Percentage_Votes_1"] + ", " + data[key]["2009_Position_1"] + ", " +data[key]["2009_Candidate_Name_2"] + ", " + data[key]["2009_Party_2"] + ", " + data[key]["2009_Votes_2"] + ", " + data[key]["2009_Percentage_Votes_2"] + ", " + data[key]["2009_Position_2"] + ", " +data[key]["2009_Candidate_Name_3"] + ", " + data[key]["2009_Party_3"] + ", " + data[key]["2009_Votes_3"] + ", " + data[key]["2009_Percentage_Votes_3"] + ", " + data[key]["2009_Position_3"] + ", " +data[key]["2009_Candidate_Name_4"] + ", " + data[key]["2009_Party_4"] + ", " + data[key]["2009_Votes_4"] + ", " + data[key]["2009_Percentage_Votes_4"] + ", " + data[key]["2009_Position_4"]
     if("2009_Candidate_Name_5" in data[key].keys()):
@@ -36,8 +36,8 @@ for key in data:
     else:
         d += "\n"
 fw.write(d)
-    
-    
+
+
 
     
      
