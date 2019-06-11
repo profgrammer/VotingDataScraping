661ae270d2f124bf76a0465ab7229468a79bd38d
Divya Khetan <divya.khetan@somaiya.edu>
2019-06-11 10:12:24 +0530

    Update pie.py
    multiple pie charts - one from each row from csv. 
    using matplotlib library of python. 
    year - 2009.

--- pie.py
+++ pie.py
@@ -10,24 +10,29 @@ lr = list(file)
 party = []
 votes = []
 
-party.append(lr[1][3])
-party.append(lr[1][8])
-party.append(lr[1][13])
-party.append(lr[1][18])
-party.append(lr[1][23])
+#plt.rcParams['font.sans-serif'] = "Montserrats"
+#plt.rcParams['font.family'] = "sans-serif"
 
-votes.append(lr[1][4])
-votes.append(lr[1][9])
-votes.append(lr[1][14])
-votes.append(lr[1][19])
-votes.append(lr[1][24])
-print(party)
-print(votes)
-plt.subplot(121)
-plt.pie(votes, labels=party)
-plt.axis('equal') # make the pie chart circular
-plt.subplot(122)
-plt.pie(votes, labels=party)
-plt.axis('equal') # make the pie chart circular
-plt.show()
+for j in range(1, len(lr)):
+    row = lr[j]
+    party.clear()
+    votes.clear()
+    party.append(row[3] + " : Rank " + row[6])
+    party.append(row[8] + " : Rank " + row[11])
+    party.append(row[13] + " : Rank " + row[16])
+    party.append(row[18] + " : Rank " + row[21])
+    party.append(row[23] + " : Rank " + row[36])
+
+    votes.append(row[4])
+    votes.append(row[9])
+    votes.append(row[14])
+    votes.append(row[19])
+    votes.append(row[24])
+    print(party)
+    print(votes)
+    plt.pie(votes, labels=party, autopct='%.0f')
+    plt.axis('equal') # make the pie chart circular
+    str1 = str(row[0]) + '_2009' + '.png'
+    plt.savefig(str1, bbox_inches='tight')
+    plt.close()#prevents overlapping pie charts
 
