495305f2cf6a02727ac9477ad35eb5efaae54a05
Divya Khetan <divya.khetan@somaiya.edu>
2019-07-04 14:45:59 +0530

    Customized text(labels) in pie chart
    Used Autopct.

--- pie_2009.py
+++ pie_2009.py
@@ -9,6 +9,7 @@ lr = list(file)
 
 party = []
 votes = []
+percent = []
 
 #plt.rcParams['font.sans-serif'] = "Montserrats"
 #plt.rcParams['font.family'] = "sans-serif"
@@ -16,11 +17,26 @@ votes = []
 c = {"BJP" : '#F47216', "INC" : "#0B8902", "NCP" : "#154A9A", "SHS" : "#FF69B4", "Others" : "#CCCCCC", "NOTA" : "#FFC00F", "IND" : "#ff0000" , "MNS" : "#681A1A", "None": "#ffe5b4", "VBA" : "#78398B", "AIMIM": "#4D4E4A", "SWP" : "#11ACB4"}
 
 color = []
+
+index = -1
+
+def func(pct, allvals):
+    global index
+    index = index + 1
+    print(index)
+    print(percent)
+    #print(pct + " " + percent[index])
+    return percent[index]
+    
+    
+
 for j in range(1, len(lr)):
     row = lr[j]
+    index = -1
     party.clear()
     votes.clear()
     color.clear()
+    percent.clear()
     party.append(row[3] + " : Rank " + row[6])
     party.append(row[8] + " : Rank " + row[11])
     party.append(row[13] + " : Rank " + row[16])
@@ -36,10 +52,15 @@ for j in range(1, len(lr)):
     color.append(c[row[13].strip()])
     color.append(c[row[18].strip()])
     color.append(c[row[23].strip()])
+    percent.append(row[5])
+    percent.append(row[10])
+    percent.append(row[15])
+    percent.append(row[20])
+    percent.append(row[25])
 
     print(party)
     print(votes)
-    plt.pie(votes, labels=party, autopct='%1.0f%%', colors = color)
+    plt.pie(votes, labels=party, autopct=lambda pct: func(pct, votes), colors = color)
     plt.axis('equal') # make the pie chart circular
     str1 = str(row[0]) + '_2009' + '.png'
     plt.savefig(str1, bbox_inches='tight')
